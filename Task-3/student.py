64b459ea348743e3f14381bcdc7bc00f88115795
Norhan Ahmed <112608294+Norhanahmed38@users.noreply.github.com>
2022-11-30 23:17:48 +0200

    Update student.py

--- Task-3/student.py
+++ Task-3/student.py
@@ -1,48 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[7]:
-
-
-
-
-students = []
-
-scores = []
-
-for N in range(int(input())):
-
-    name = input()
-
-    score = float(input())
-
-    scores.append(score)
-
-    students.append([name, score])
-
-    
-
-        
-
-count = scores.count(min(scores))
-
-for i in range(count):
-
-    scores.remove(min(scores))
-
-        
-
-    secondHigh = min(scores)
-
-    
-
-    students.sort()
-
-    output = [x for x in students if x[1] == secondHigh]
-
-    for i in output:
-
-        print(i[0])
 
 
 # In[13]:
@@ -69,50 +27,7 @@ names = [name for name,marks in sorted(marksheet) if marks == second_lowest]
 print('\n'.join(names))
 
 
-# In[11]:
 
-
-
-students = []
-
-scores = []
-
-for N in range(int(input())):
-
-    name = input()
-
-    score = float(input())
-
-    scores.append(score)
-
-    students.append([name, score])
-
-
-
-
-
-count = scores.count(min(scores))
-
-for i in range(count):
-
-    scores.remove(min(scores))
-
-
-
-secondHigh = min(scores)
-
-
-
-students.sort()
-
-output = [x for x in students if x[1] == secondHigh]
-
-for i in output:
-
-    print(i[0])
-
-
-# In[ ]:
 
 
 
